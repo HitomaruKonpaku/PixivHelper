d0583e1566606909922bbfb88e7cf2fdd6c37ac0
HitomaruKonpaku <HitomaruKonpaku@users.noreply.github.com>
2017-10-04 02:53:39 +0700

    Update UI base on location

--- js/_main.js
+++ js/_main.js
@@ -1,15 +1,25 @@
 console.info('PixivHelper running...')
 
 $(document).ready(() => {
+    var uri = URI(window.location)
+    var qs = uri.search(true)
     var p = phPan()
-
-    p
-        .insertAfter($('._user-profile-card'))
         .append(phTitle)
-        .append(phButtonPan)
 
-    // run animation
-    p.hide().fadeIn()
+    if (uri.path().indexOf('member_illust') != -1) {
+        var bp = phButtonPan()
+
+        if (qs.id) {
+            bp.append(phButtonOpenWorks)
+        } else if (qs.mode && qs.illust_id) {
+            bp.append(phButtonDownload)
+        }
+
+        p
+            .append(bp)
+            .insertAfter($('._user-profile-card'))
+            .hide().fadeIn()
+    }
 })
 
 function phPan() {
@@ -26,18 +36,23 @@ function phTitle() {
 function phButtonPan() {
     return $('<div/>')
         .addClass('_follow-buttons')
-        .append(phButtonDownload)
-        .append(phButtonOpenAllWorks)
 }
 
 function phButtonDownload() {
     return $('<div/>')
+        .attr('id', 'phi-download')
         .addClass('follow-button js-follow-button')
         .text('Download')
+        .click(() => {
+            // $('#phi-download').hide()
+        })
 }
 
-function phButtonOpenAllWorks() {
+function phButtonOpenWorks() {
     return $('<div/>')
         .addClass('follow-button js-follow-button')
-        .text('Open all works')
+        .text('Open works')
+        .click(() => {
+
+        })
 }
